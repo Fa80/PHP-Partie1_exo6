997f3207431653414fb47083b666bf43abf57d2f
root <ngomfa@gmail.com>
2018-11-16 15:52:40 +0100

    exo6

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo6</title>
+</head>
+<body>
+
+    <?php
+    //exo: 1-Créer une variable name et l'initialiser avec la valeur de votre choix.
+    //2-Afficher : "Bonjour" + name + ", comment vas tu ?".
+
+    $name = "John";
+      echo "Bonjour" . " " . $name . " " . "comment vas tu ?";
+
+  ?>
+</body>
+</html>
